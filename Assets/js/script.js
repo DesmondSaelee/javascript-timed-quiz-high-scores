b317d5ad17dc9afa84a97f3d64113709dd29a8c8
Brianna Ligon <desmondsaelee@hotmail.com>
2023-03-14 21:38:16 -0700

    Bcs Push

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,5 @@
-// --- old code---
-// var startButton = document.getElementById("startButton");
-// var quizQuestion1 = document.getElementById("question1");
-// var intro = document.getElementById("openingPage");
-
-// var questionindex = 0;
-// var quizQuestion2 = document.getElementById("question2");
-// var quizQuestion3 = document.getElementById("question3");
-// var quizQuestion4 = document.getElementById("question4");
-// var endPage = document.getElementById("endPage");
+// how to set highscores using local storage
+// how to stop timer at current time. 
  
 // -----new code----
 var openingPage = document.querySelector("#openingPage");
@@ -50,17 +42,10 @@ var questionsArray = [
     },
 ];
 
-// document.getElementById('answer2').style.visibility= 'hidden';
+
 function init(){
     timerCount = 75
-    // var x = document.getElementById("answer2");
-    // document.getElementById('answer2').style.visibility= 'hidden';
-//    this code hides the answer2 button, but I need to figure out how to bring it back.
-    // if (x.style.display === "none") {
-    //   x.style.display = "block";
-    // } else {
-    //   x.style.display = "none";
-    // }
+  
     
 };
 
@@ -111,12 +96,18 @@ function startTimer() {
   }
 
   function displayResults(){
-    finalScore.textContent = "Your final score is" + timerCount;
+    finalScore.textContent = "Your final score is" + " " + timerCount;
     quizPage.classList.add("hidden")
     scorePage.classList.remove("hidden")
 
+    function stopCount() {
+        clearTimeout(timerClock);
+        timerCount = timerCount.value;
+      }
+      stopCount();
+
     };
-// need to finish highscores function to create list for highscores and only display at end.
+
     function displayHighScores(){
         highScorePage.classList.remove("hidden")
         
@@ -129,8 +120,7 @@ function startTimer() {
         currentQuestion++ 
         answersEl.innerHTML = ""
         if(currentQuestion >= questionsArray.length){
-            // have display results function to hide other screens and shows score. make another div
-            // to hide then view questions when you click start quiz hide opening page
+         
             displayResults()
         } else{
             displayQuestion()
@@ -138,22 +128,16 @@ function startTimer() {
        
     } else {
         timerCount-=5
-        // currentQuestion++
-        // answersEl.innerHTML = ""
-        // displayQuestion()
+      
     }
+
+  
    
 
   }
 
 
-//   function question2(){
-//     question.textContent = "The condition in and if/else statement is enclosed within ___."
-//     answer1.textContent = "quotes";
-//     answer2.textContent = "curly brackets";
-//     answer3.textContent = "parentheses";
-//     answer4.textContent = "square brackets";
-//   }
+
 
   startQuiz.addEventListener("click", startGame);
 
@@ -163,185 +147,4 @@ function startTimer() {
 
   init();
 
-// ---old code---
-// when user answer question correctly index goes up 1
 
-
-
-
-// --- old code ---
-// function printquestions() {
-//     console.log(questions[questionindex]);
-
-//     document.createElement("h2");
-//     var questionprompt = questions[0].question;
-//     var question = document.createElement("h2");
-//     question.textContent = questionprompt;
-//     quizQuestion1.append(questionprompt);
-
-
-//     document.createElement("button");
-//     var choice1Btn = document.createElement("button");
-//     choice1Btn.textContent = questions[0].choice1;
-//     quizQuestion1.append(choice1Btn);
-//     choice1Btn.style.backgroundColor = "blueviolet";
-
-//     document.createElement("button");
-//     var choice2Btn = document.createElement("button");
-//     choice2Btn.textContent = questions[0].choice2;
-//     quizQuestion1.append(choice2Btn);
-//     choice2Btn.style.backgroundColor = "blueviolet"
-
-//     document.createElement("button");
-//     var correctAnswerBtn = document.createElement("button");
-//     correctAnswerBtn.textContent = questions[0].correctAnswer;
-//     quizQuestion1.append(correctAnswerBtn);
-//     correctAnswerBtn.style.backgroundColor = "blueviolet"
-
-//     document.createElement("button");
-//     var choice3Btn = document.createElement("button");
-//     choice3Btn.textContent = questions[0].choice3;
-//     quizQuestion1.append(choice3Btn);
-//     choice3Btn.style.backgroundColor = "blueviolet"
-
-//     correctAnswerBtn.addEventListener("click", function () {
-//         quizQuestion1.style.display = "none"
-//         quizQuestion2.style.display = "block"
-//     })
-
-//     document.createElement("h2");
-//     var questionprompt = questions[1].question;
-//     var question = document.createElement("h2");
-//     question.textContent = questionprompt;
-//     quizQuestion2.append(questionprompt);
-
-//     document.createElement("button");
-//     var choice1Btn = document.createElement("button");
-//     choice1Btn.textContent = questions[1].choice1;
-//     quizQuestion2.append(choice1Btn);
-//     choice1Btn.style.backgroundColor = "blueviolet";
-
-//     document.createElement("button");
-//     var choice2Btn = document.createElement("button");
-//     choice2Btn.textContent = questions[1].choice2;
-//     quizQuestion2.append(choice2Btn);
-//     choice2Btn.style.backgroundColor = "blueviolet";
-
-//     document.createElement("button");
-//     var correctAnswerBtn = document.createElement("button");
-//     correctAnswerBtn.textContent = questions[1].correctAnswer;
-//     quizQuestion2.append(correctAnswerBtn);
-//     correctAnswerBtn.style.backgroundColor = "blueviolet";
-
-//     document.createElement("button");
-//     var choice3Btn = document.createElement("button");
-//     choice3Btn.textContent = questions[1].choice3;
-//     quizQuestion2.append(choice3Btn);
-//     choice3Btn.style.backgroundColor = "blueviolet";
-
-//     correctAnswerBtn.addEventListener("click", function () {
-//         quizQuestion2.style.display = "none"
-//         quizQuestion3.style.display = "block"
-//     })
-
-//     document.createElement("h2");
-//     var questionprompt = questions[2].question;
-//     var question = document.createElement("h2");
-//     question.textContent = questionprompt;
-//     quizQuestion3.append(questionprompt);
-
-
-//     document.createElement("button");
-//     var choice1Btn = document.createElement("button");
-//     choice1Btn.textContent = questions[2].choice1;
-//     quizQuestion3.append(choice1Btn);
-//     choice1Btn.style.backgroundColor = "blueviolet";
-
-//     document.createElement("button");
-//     var choice2Btn = document.createElement("button");
-//     choice2Btn.textContent = questions[2].choice2;
-//     quizQuestion3.append(choice2Btn);
-//     choice2Btn.style.backgroundColor = "blueviolet";
-
-//     document.createElement("button");
-//     var choice3Btn = document.createElement("button");
-//     choice3Btn.textContent = questions[2].choice3;
-//     quizQuestion3.append(choice3Btn);
-//     choice3Btn.style.backgroundColor = "blueviolet";
-
-
-//     document.createElement("button");
-//     var correctAnswerBtn = document.createElement("button");
-//     correctAnswerBtn.textContent = questions[2].correctAnswer;
-//     quizQuestion3.append(correctAnswerBtn);
-//     correctAnswerBtn.style.backgroundColor = "blueviolet";
-
-//     correctAnswerBtn.addEventListener("click", function () {
-//         quizQuestion3.style.display = "none"
-//         quizQuestion4.style.display = "block"
-//     })
-
-
-//     document.createElement("h2");
-//     var questionprompt = questions[3].question;
-//     var question = document.createElement("h2");
-//     question.textContent = questionprompt;
-//     quizQuestion4.append(questionprompt);
-
-//     document.createElement("button");
-//     var choice1Btn = document.createElement("button");
-//     choice1Btn.textContent = questions[3].choice1;
-//     quizQuestion4.append(choice1Btn);
-//     choice1Btn.style.backgroundColor = "blueviolet";
-
-//     document.createElement("button");
-//     var choice2Btn = document.createElement("button");
-//     choice2Btn.textContent = questions[3].choice2;
-//     quizQuestion4.append(choice2Btn);
-//     choice2Btn.style.backgroundColor = "blueviolet";
-
-//     document.createElement("button");
-//     var correctAnswerBtn = document.createElement("button");
-//     correctAnswerBtn.textContent = questions[3].correctAnswer;
-//     quizQuestion4.append(correctAnswerBtn);
-//     correctAnswerBtn.style.backgroundColor = "blueviolet";
-
-//     document.createElement("button");
-//     var choice3Btn = document.createElement("button");
-//     choice3Btn.textContent = questions[3].choice3;
-//     quizQuestion4.append(choice3Btn);
-//     choice3Btn.style.backgroundColor = "blueviolet";
-
-//     correctAnswerBtn.addEventListener("click", function () {
-//         quizQuestion4.style.display = "none"
-//         endPage.style.display = "block"
-//     })
-
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-// ---old code---
-
-
-// startButton.addEventListener("click", function () {
-//     // console.log("clicked")
-//     // console.log(quizcontainer)
-//     // console.log(intro)
-//     intro.style.display = "none"
-//     quizQuestion1.style.display = "block"
-//     printquestions()
-//     //hide intro div. 
-//     // show quiz
-//     // initialize timer
-
-// });
